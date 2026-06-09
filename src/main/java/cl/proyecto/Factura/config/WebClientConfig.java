8e385c45dd92e35cbb2ae820f9360e3e1155d148
Matías <ma.beas@duocuc.cl>
2026-06-08 23:30:49 -0400

    Lo mismo

--- src/main/java/cl/proyecto/Factura/config/WebClientConfig.java
+++ src/main/java/cl/proyecto/Factura/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package cl.proyecto.Factura.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,19 +8,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${webclient.paciente.url}")
+    private String pacienteUrl;
+
+    @Value("${webclient.hospitalizacion.url}")
+    private String hospitalizacionUrl;
+
     @Bean
     public WebClient webClientPaciente() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8082")
+                .baseUrl(pacienteUrl)
                 .build();
     }
 
     @Bean
     public WebClient webClientHospitalizacion() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8282")
+                .baseUrl(hospitalizacionUrl)
                 .build();
     }
-
-
 }
